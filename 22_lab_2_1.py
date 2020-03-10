a8a4b68d0812c46891a8910f9fc3630555bd9f40
vlad <stanevichvlad02@gmail.com>
2020-03-10 20:59:12 +0300

    считает сумма частей

--- 22_lab_2_1.py
+++ 22_lab_2_1.py
@@ -1,2 +1,9 @@
-my_list = [int(s) for s in input("Введи элементы массива через пробел:/n").split()]
-print(my_list)
+import math
+
+my_list = [int(s) for s in input("Введи элементы массива через пробел: ").split()]
+number_box = math.ceil(math.sqrt(len(my_list)))
+length_box = math.ceil(len(my_list) / number_box)
+sum = [0]*number_box
+for i in range(len(my_list)):
+    sum[i//length_box] += my_list[i]
+print(sum)
