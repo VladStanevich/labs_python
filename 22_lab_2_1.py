4581c6c7eec2958b808409d199227ff1e8c81af6
vlad <stanevichvlad02@gmail.com>
2020-03-11 09:38:29 +0300

    считает сумму блоков внутри диапозона

--- 22_lab_2_1.py
+++ 22_lab_2_1.py
@@ -6,4 +6,12 @@ length_box = math.ceil(len(my_list) / number_box)
 sum = [0]*number_box
 for i in range(len(my_list)):
     sum[i//length_box] += my_list[i]
-print(sum)
+l = int(input("Введи левую границу: "))
+r = int(input("Введи правую границу: "))
+summa = 0
+for i in range(len(sum)):
+    if i <= l//3 or i >= r//3:
+        continue
+    else:
+        summa += sum[i]
+print(summa)
