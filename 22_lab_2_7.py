034af7b18375a8cabcaf1c3d158508ca972ff76e
vlad <stanevichvlad02@gmail.com>
2020-03-15 15:53:11 +0300

    работает

--- 22_lab_2_7.py
+++ 22_lab_2_7.py
@@ -1,9 +1,15 @@
 def leonardo_number(n):
-    if n == 0 or n == 1:
+    if n == 1 or n == 2:
         return 1
-    else:
-        return leonardo_number(n-1) + leonardo_number(n-2) + 1
+    num1 = num2 = 1
+    i = 2
+    while i < n:
+        l_num = num1 + num2 + 1
+        num1 = num2
+        num2 = l_num
+        i += 1
+    return l_num
 
 
-for i in range(10):
+for i in range(1,101):
     print(leonardo_number(i), end=" ")
