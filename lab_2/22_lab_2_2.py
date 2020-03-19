ed456664c574f1014be091f283276856f46d0521
vlad <stanevichvlad02@gmail.com>
2020-03-19 16:05:45 +0300

    убрал лишний пробел в конце строки

--- lab_2/22_lab_2_2.py
+++ lab_2/22_lab_2_2.py
@@ -6,21 +6,22 @@ def create_word(len):
     word = ''.join(random.choice(list(string.ascii_letters)) for x in range(len))
     return word
 
-def create_line(len):
-    stroka = ''
-    for x in range(len):
-        stroka += create_word(random.randint(3,10))
-        stroka += ' '
-    return stroka
-
-def create_file(size):
-    f = open("output.txt", "w")
-    sz = os.path.getsize("output.txt")
+def create_file(size, K=(10,100), L=(3,10)):
+    f = open("lab_2/output.txt", "w")
+    sz = os.path.getsize("lab_2/output.txt")
     while sz < size:
-        f.write(create_line(random.randint(10,100))+"\n")
-        sz = os.path.getsize("output.txt")
+        stroka = random.randint(*K)
+        for i in range(stroka):
+            f.write(create_word(random.randint(*L)))
+            sz = os.path.getsize("lab_2/output.txt")
+            if sz >= size:
+                break
+            if i == stroka-1:
+                f.write("\n")
+            else:
+                f.write(" ") 
     f.close()
 
 
 if __name__ == "__main__":
-    create_file(1024**2)
+    create_file(1024)
