65b1136882c913dc1ff9015778e6b1c47d5cf2d7
vlad <stanevichvlad02@gmail.com>
2020-03-19 15:59:00 +0300

    pep 8

--- lab_2/22_lab_2_4.py
+++ lab_2/22_lab_2_4.py
@@ -1,11 +1,11 @@
-def flatten(l,a=[]):
+def flatten(l, a=[]):
     for elem in l:
         if type(elem) == list:
-            flatten(elem,a)
+            flatten(elem, a)
         else:
             a.append(elem)
     return a
 
     
 l =  [1,2,[3,4,5],[6,[7,8]]]
-print(flatten(l,[]))
+print(flatten(l))
