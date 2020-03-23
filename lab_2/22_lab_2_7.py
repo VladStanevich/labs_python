210acf5e471307b77000b9db8d8ef9d9d1ac44f1
vlad <stanevichvlad02@gmail.com>
2020-03-23 12:20:23 +0300

    проверка входных данных

--- lab_2/22_lab_2_7.py
+++ lab_2/22_lab_2_7.py
@@ -1,15 +1,21 @@
 def leonardo_number(n):
-    if n == 1 or n == 2:
-        return 1
-    num1 = num2 = 1
-    i = 2
-    while i < n:
-        l_num = num1 + num2 + 1
-        num1 = num2
-        num2 = l_num
-        i += 1
-    return l_num
+    try:
+        if n < 0 or type(n) is not int:
+            print("Вы ввели не натуральное число")
+        elif n == 1 or n == 2:
+            return 1
+        else:
+            num1 = num2 = 1
+            i = 2
+            while i < n:
+                l_num = num1 + num2 + 1
+                num1 = num2
+                num2 = l_num
+                i += 1
+            return l_num
+    except:
+        print("Вы ввели не число")
 
 
-for i in range(1,101):
-    print(leonardo_number(i), end=" ")
+if __name__ == "__main__":
+    print(leonardo_number(1.5))
