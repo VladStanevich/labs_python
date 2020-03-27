a39bf1ea480761df8c80a08f4510b02d2d0a11a2
vlad <stanevichvlad02@gmail.com>
2020-03-27 14:38:44 +0300

    поменял type на isinstance

--- lab_2/22_lab_2_4.py
+++ lab_2/22_lab_2_4.py
@@ -1,11 +1,11 @@
 def flatten(l, a=[]):
     for elem in l:
-        if type(elem) == list:
+        if isinstance(elem, list):
             flatten(elem, a)
         else:
             a.append(elem)
     return a
 
     
-l =  [1,2,[3,4,5],[6,[7,8]]]
+l = [[1,[2]], [3, 3], [[3,[[[[[[[[[44,[[[[[[[[[[[[[[[[[333,[[[[[[[[[[[[[[[[[[[[[[[[[55]]]]]]]]]]]]]]]]]]]]]]]]]]]]]],76835]]]]]]]]]]]]]]]]]]]],[6,5,2]]]], [[[6]]]]
 print(flatten(l))
