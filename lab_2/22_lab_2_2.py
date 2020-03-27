81d824f640da12cdadad9729492cea0b51fb8193
vlad <stanevichvlad02@gmail.com>
2020-03-27 20:08:55 +0300

    пишет в консоли процент заполнения

--- lab_2/22_lab_2_2.py
+++ lab_2/22_lab_2_2.py
@@ -2,26 +2,31 @@ import random
 import string
 import os.path
 
+
 def create_word(len):
     word = ''.join(random.choice(list(string.ascii_letters)) for x in range(len))
     return word
 
 def create_file(size, K=(10,100), L=(3,10)):
-    f = open("lab_2/output.txt", "w")
-    sz = os.path.getsize("lab_2/output.txt")
+    sz = 0
+    size *= 1024**2
+    f = open("lab_2/output.txt", 'w')
+    f.seek(0)
+    f.close() 
     while sz < size:
         stroka = random.randint(*K)
+        f = open("lab_2/output.txt", 'a') 
         for i in range(stroka):
             f.write(create_word(random.randint(*L)))
-            sz = os.path.getsize("lab_2/output.txt")
-            if sz >= size:
-                break
-            if i == stroka-1:
+            if i == (stroka - 1):
                 f.write("\n")
             else:
-                f.write(" ") 
-    f.close()
+                f.write(" ")
+        f.close()
+        sz = os.path.getsize("lab_2/output.txt")
+        print("{} %".format(sz*100//size))
+    print("Complited!")
 
 
 if __name__ == "__main__":
-    create_file(1024)
+    create_file(1)
